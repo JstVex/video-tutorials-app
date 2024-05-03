74345cf59cfdfbdb9bb0daa693398d7140064f8c
Lu Lu, Hein Htet <heinhtetlulu14@gmail.com>
2024-05-03 01:19:53 -0700

    Update data

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -8,8 +8,8 @@ export interface Video {
 }
 
 const videos: Video[] = [
-    { id: 1, title: 'React Tutorial', description: 'Learn to build websites with React!', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "/thumbnails/react-thumbnail.png" },
-    { id: 2, title: 'HTML The Basics ', description: 'Learn the basics of HTML and its usage', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "https://img.youtube.com/vi/Z1neJhEEoFA/0.jpg" },
+    { id: 1, title: 'React Tutorial', description: 'Learn the basic features of React and how we can build websites using React', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "/thumbnails/react-thumbnail.png" },
+    { id: 2, title: 'HTML The Basics ', description: 'Learn the basics of HyperText Markup Language (HTML)', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "https://img.youtube.com/vi/Z1neJhEEoFA/0.jpg" },
     { id: 3, title: 'CSS Flexbox', description: 'Learn one of the most used properities of CSS: Flexbox', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "https://img.youtube.com/vi/Z1neJhEEoFA/0.jpg" },
     { id: 4, title: 'Backend with Node and Express', description: 'Learn how to create server and routes for your backend', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "https://img.youtube.com/vi/Z1neJhEEoFA/0.jpg" },
 ];
