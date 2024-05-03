1df2555feb8a7e03b0d2e6d58e3b8b9769370cfa
theFool <josephcgarcia1001@gmail.com>
2024-05-03 11:15:02 -0700

    changed thumbnail

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -8,10 +8,10 @@ export interface Video {
 }
 
 const videos: Video[] = [
-    { id: 1, title: 'React Tutorial', description: 'Learn the basic features of React and how we can build websites using React', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "/thumbnails/react-thumbnail.png" },
-    { id: 2, title: 'HTML The Basics ', description: 'Learn the basics of HyperText Markup Language (HTML)', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "https://img.youtube.com/vi/Z1neJhEEoFA/0.jpg" },
-    { id: 3, title: 'CSS Flexbox', description: 'Learn one of the most used properities of CSS: Flexbox', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "https://img.youtube.com/vi/Z1neJhEEoFA/0.jpg" },
-    { id: 4, title: 'Backend with Node and Express', description: 'Learn how to create server and routes for your backend', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "https://img.youtube.com/vi/Z1neJhEEoFA/0.jpg" },
+    { id: 1, title: 'React Tutorial', description: 'Learn the basic features of React and how we can build websites using React', date: '01 May 2024', videoId: '6Gi_MPmazC0', thumbNail: "/thumbnails/react-thumbnail.png" },
+    { id: 2, title: 'HTML The Basics ', description: 'Learn the basics of HyperText Markup Language (HTML)', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "https://img.youtube.com/vi/Z1neJhEEoFA/hqdefault.jpg" },
+    { id: 3, title: 'CSS Flexbox', description: 'Learn one of the most used properities of CSS: Flexbox', date: '01 May 2024', videoId: 'TohziRysKKY', thumbNail: "https://img.youtube.com/vi/TohziRysKKY/hqdefault.jpg" },
+    { id: 4, title: 'Backend with Node and Express', description: 'Learn how to create server and routes for your backend', date: '01 May 2024', videoId: 'jCScjzA2rzg', thumbNail: "https://img.youtube.com/vi/jCScjzA2rzg/hqdefault.jpg" },
 ];
 
 export const getVideos = () => videos;
