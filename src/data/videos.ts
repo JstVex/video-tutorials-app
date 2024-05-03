e86a1597321929e9580b3281d61c1a26b6d537d1
Lu Lu, Hein Htet <heinhtetlulu14@gmail.com>
2024-05-03 00:52:04 -0700

    Data for videos

---
+++ src/data/videos.ts
@@ -0,0 +1,19 @@
+export interface Video {
+    id: number;
+    title: string;
+    description: string;
+    date?: string;
+    videoId: string;
+    thumbNail: string;
+}
+
+const videos: Video[] = [
+    { id: 1, title: 'React Tutorial', description: 'Learn to build websites with React!', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "/thumbnails/react-thumbnail.png" },
+    { id: 2, title: 'HTML The Basics ', description: 'Learn the basics of HTML and its usage', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "https://img.youtube.com/vi/Z1neJhEEoFA/0.jpg" },
+    { id: 3, title: 'CSS Flexbox', description: 'Learn one of the most used properities of CSS: Flexbox', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "https://img.youtube.com/vi/Z1neJhEEoFA/0.jpg" },
+    { id: 4, title: 'Backend with Node and Express', description: 'Learn how to create server and routes for your backend', date: '01 May 2024', videoId: 'Z1neJhEEoFA', thumbNail: "https://img.youtube.com/vi/Z1neJhEEoFA/0.jpg" },
+];
+
+export const getVideos = () => videos;
+
+export const getVideo = (id: number) => videos.find(video => video.id === id);
